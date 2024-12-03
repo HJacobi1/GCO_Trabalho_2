3e674c88ae66c249d7a015d4b28a4c4ba35ba3be
HJacobi1 <henrique.jacobi1@gmail.com>
2024-12-02 22:22:49 -0300

    MIgrations

---
+++ sql_scripts/20241201_migration_inicial.sql
@@ -0,0 +1,8 @@
+CREATE TABLE TabelaTeste(
+    id INT PRIMARY KEY,
+    data_insercao Date,
+    descricao varchar(100)
+);
+
+INSERT INTO TabelaTeste VALUES (1,'2024-12-01','teste_migration');
+INSERT INTO TabelaTeste VALUES (2,'2024-12-01','teste_migration2');
---
+++ sql_scripts/20241202_migration_dois.sql
@@ -0,0 +1,2 @@
+INSERT INTO TabelaTeste VALUES (3,'2024-12-02','teste_migration3');
+INSERT INTO TabelaTeste VALUES (4,'2024-12-02','teste_migration4');
