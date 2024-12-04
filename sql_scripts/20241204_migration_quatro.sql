e994b553d9f2ae60c73c12767eb1b5603ad9ae53
HJacobi1 <146138990+HJacobi1@users.noreply.github.com>
2024-12-04 20:32:11 -0300

    Commit Apresentação

---
+++ sql_scripts/20241204_migration_quatro.sql
@@ -0,0 +1,8 @@
+CREATE TABLE OutraTabela(
+    id INT PRIMARY KEY,
+    data_insercao Date,
+    descricao varchar(100)
+);
+
+INSERT INTO OutraTabela VALUES (1,'2024-12-01','migracao_Ap1');
+INSERT INTO OutraTabela VALUES (2,'2024-12-01','migracao_Ap2');
